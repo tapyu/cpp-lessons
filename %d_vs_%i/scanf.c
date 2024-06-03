3f8a24810c23faf25c48e0ec0a159ef5606eaad8
Rubem Pacelli <rubem.engenharia@gmail.com>
2024-06-03 20:30:44 +0200

    Update scanf.c

--- %d_vs_%i/scanf.c
+++ %d_vs_%i/scanf.c
@@ -3,17 +3,17 @@
 int main() {
     int num;
 
-    printf("Testing for %%i:\n");
+    printf("scanf with %%i:\n");
     printf("Enter  42, 052, or 0x2A (either one will result in 42 in decimal): ");
     scanf("%i", &num);
-    printf("The number (in decimal) using %%i is: %i\n", num);
-    printf("The number (in decimal) using %%d is: %d\n", num);
+    printf("printf with %%i is: %i\n", num);
+    printf("printf with %%d is: %d\n", num);
     
-    printf("Testing for %%d:\n");
+    printf("scanf with %%d:\n");
     printf("Enter  42, 052, or 0x2A (entering 052 or 0x2A will not work correctly, only 42 will): ");
     scanf("%d", &num);
-    printf("The number (in decimal) using %%i is: %i\n", num);
-    printf("The number (in decimal) using %%d is: %d\n", num);
+    printf("printf with %%i is: %i\n", num);
+    printf("printf with %%d is: %d\n", num);
     
     return 0;
 }
