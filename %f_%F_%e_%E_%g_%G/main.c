b4c6641d7a564bfb487e4a8322bdfc5e743f7c57
Rubem Pacelli <rubem.engenharia@gmail.com>
2024-06-04 05:19:49 +0200

    Update main.c

--- %f_%F_%e_%E_%g_%G/main.c
+++ %f_%F_%e_%E_%g_%G/main.c
@@ -7,6 +7,8 @@ int main() {
     printf("Using %%F: %F\n", num);    // Fixed-point notation. Identical to `%f`, unless when `num` is `nan` or `inf` (from `math.h`). In this case, the string is printed in uppercase instead of lowercase. -> 12345.678900
     printf("Using %%e: %e\n", num);    // Scientific notation (lowercase e) -> 1.234568e+04
     printf("Using %%E: %E\n", num);    // Scientific notation (uppercase E) -> 1.234568E+04
+    printf("Using %%a: %a\n", num);    // Hexadecimal notation: 0x1.81cd6e631f8a1p+13
+    printf("Using %%A: %A\n", num);    // Hexadecimal notation: 0X1.81CD6E631F8A1P+13
     printf("Using %%g: %g\n", num);    // Shortest representation between %e and %f (approximates the floating part if the exponent is less than 4) -> 12345.7
     printf("Using %%G: %G\n", num);    // Shortest representation between %E and %f (approximates the floating part if the exponent is less than 4) -> 12345.7
 
