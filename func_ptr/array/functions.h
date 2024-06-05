2b67eaa473f27a76b23e60d91f0a54693e74dbb7
Rubem Pacelli <rubem.engenharia@gmail.com>
2024-06-05 18:03:44 +0200

    Create functions.h

---
+++ func_ptr/array/functions.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+// Function prototypes
+void add(int a, int b);
+void subtract(int a, int b);
+void multiply(int a, int b);
+void divide(int a, int b);
+
+#endif // FUNCTIONS_H
