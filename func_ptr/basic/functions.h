c6da02c3737c6b97c8f512c02b94c5ca9638e0c1
Rubem Pacelli <rubem.engenharia@gmail.com>
2024-06-05 18:00:27 +0200

    Create functions.h

---
+++ func_ptr/basic/functions.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+// Function prototypes
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
+
+#endif // FUNCTIONS_H
