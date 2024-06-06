c4097fb10c271bb8186a6ffb47bb66b3666f93b7
Rubem Pacelli <rubem.engenharia@gmail.com>
2024-06-06 04:03:56 +0200

    Update main.c

--- union_vs_struct/main.c
+++ union_vs_struct/main.c
@@ -5,21 +5,21 @@ int main() {
     union my_union {
         int a;   // can't use both a and b at once
         char b;
-    } foo;
+    } foo; // we are instantiating a variable named `foo` from `my_union`
 
-    printf("union!");
+    printf("union!\n");
     foo.a = 3; // OK
     printf("1st %d and %c\n", foo.a, foo.b);
     foo.b = 'c'; // NO! this affects the value of foo.a!
     printf("2nd %d and %c\n", foo.a, foo.b);
     
     // struct
-    struct bar {
+    struct my_struct {
         int a;   // can use both a and b simultaneously
         char b;
-    } bar;
+    } bar; // we are instantiating a variable named `bar` from `my_struct`
 
-    printf("struct!");
+    printf("struct!\n");
     bar.a = 3; // OK
     printf("1st %d and %c\n", bar.a, bar.b);
     bar.b = 'c'; // NO! this affects the value of bar.a!
